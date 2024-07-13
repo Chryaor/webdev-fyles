3e4ae3ee9914b096fe011d6702bce3b8043780d5
Christy <107241938+Chryaor@users.noreply.github.com>
2024-07-13 23:30:00 +0530

    Add files via upload
    made changes to quotation, captions and forms. New form CSS

--- script.js
+++ script.js
@@ -15,15 +15,14 @@ $(document).ready(function() {
 document.querySelector('.card').classList.add('selected');
 
 function changeImage(cardElement, imageUrl) {
-  // Change the main image
   var mainImage = document.getElementById('mainImage');
   mainImage.src = imageUrl;
   
-  // Remove 'selected' class from all cards
+  // Remove selected class from all cards
   var cards = document.querySelectorAll('.card');
   cards.forEach(card => card.classList.remove('selected'));
   
-  // Add 'selected' class to the clicked card
+  // Add selected class to the clicked card
   cardElement.classList.add('selected');
 }
 
