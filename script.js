e58bf17edfa19793afb22f8533140df294f8d6d3
Christy <107241938+Chryaor@users.noreply.github.com>
2024-07-14 15:51:21 +0530

    Add files via upload
    Final changes and favicon, updated images

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.card-item').hover(
-        function() {
+        function () {
             $(this).find('.card-caption').fadeIn();
-        }, function() {
+        }, function () {
             $(this).find('.card-caption').fadeOut();
         }
     );
-    setTimeout(function() {
+    setTimeout(function () {
         $('.container-5 .card').eq(1).trigger('click');
     }, 10);
 });
@@ -15,19 +15,42 @@ $(document).ready(function() {
 document.querySelector('.card').classList.add('selected');
 
 function changeImage(cardElement, imageUrl) {
-  var mainImage = document.getElementById('mainImage');
-  mainImage.src = imageUrl;
-  
-  // Remove selected class from all cards
-  var cards = document.querySelectorAll('.card');
-  cards.forEach(card => card.classList.remove('selected'));
-  
-  // Add selected class to the clicked card
-  cardElement.classList.add('selected');
+    var mainImage = document.getElementById('mainImage');
+    mainImage.src = imageUrl;
+
+    // Remove selected class from all cards
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.classList.remove('selected'));
+
+    // Add selected class to the clicked card
+    cardElement.classList.add('selected');
 }
 
 function clearForm() {
     var form = document.getElementById('formId');
     form.reset();
     
-  }
+    var myModal = new bootstrap.Modal(document.getElementById('contactUsModal'));
+    myModal.hide();
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const indicators = document.querySelectorAll(".card-indicator img");
+    const row = document.querySelector(".container .row");
+
+    // Initialize event listeners for each indicator
+    indicators.forEach((indicator, index) => {
+        indicator.addEventListener("click", function() {
+            // Set all indicators to default (assets/images/2.svg)
+            indicators.forEach((img, idx) => {
+                img.src = "assets/images/2.svg";
+            });
+            // Set the clicked indicator to selected (assets/images/1.svg)
+            indicator.src = "assets/images/1.svg";
+            // Move the row of cards to the left
+            const offset = index * -100; // Adjust as needed based on your layout
+            row.style.transform = `translateX(${offset}%)`;
+        });
+    });
+});
